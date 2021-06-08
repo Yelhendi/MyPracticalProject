3f22e2eb299f230cbfab511101ade4f8aa1f1e98
Yasmin El-Hendi <yasminelhendi@gmail.com>
2021-06-08 19:20:19 +0100

    Service 3 done

--- code/Merge/Controllers/MergeController.cs
+++ code/Merge/Controllers/MergeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -14,6 +15,18 @@ namespace Merge.Controllers
         //Need to be able to call application
         //numbersurl: https://localhost:44345/
         //LetterUrl: https://localhost:44364/
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            //Call numbers service 
+            var numbersService = "https://localhost:44345/numbers ";
+            var numbersResponseCall = await new HttpClient().GetStringAsync(numbersService);
+            var lettersService = "https://localhost:44364/letters ";
+            var lettersResponseCall = await new HttpClient().GetStringAsync(lettersService);
 
+            //Ready to merge stuff using string interpolatrion
+            var mergedResponse = $"{numbersResponseCall}{lettersResponseCall}";
+            return Ok(mergedResponse);
+        }
     }
 }
