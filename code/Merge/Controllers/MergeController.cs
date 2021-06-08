5363437b166f5dab5673b55eaa3a5085153f253c
Yasmin El-Hendi <yasminelhendi@gmail.com>
2021-06-08 19:10:31 +0100

    Letters and numbers services

---
+++ code/Merge/Controllers/MergeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Merge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        //Need to be able to call application
+        //numbersurl: https://localhost:44345/
+        //LetterUrl: https://localhost:44364/
+
+    }
+}
