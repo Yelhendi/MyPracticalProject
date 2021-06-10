58b29abb810aa3c81e17d9b81caeede247654c6d
Yasmin El-Hendi <yasminelhendi@gmail.com>
2021-06-10 14:48:37 +0100

    Letters and numbers service ready

---
+++ code/Merge/Controllers/MergeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Merge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MergeController : ControllerBase
+    {
+        //NumbersURL: https://localhost:44365/
+        //LetterURL:https://localhost:44397/
+    }
+}
