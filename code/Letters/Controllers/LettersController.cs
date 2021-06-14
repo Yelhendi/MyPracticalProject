10f7d5ded14a235a827e13b221c5dd5879c2ea40
Yasmin El-Hendi <yasminelhendi@gmail.com>
2021-06-14 12:12:30 +0100

    Added for loop to Numbers and Letters controller

--- code/Letters/Controllers/LettersController.cs
+++ code/Letters/Controllers/LettersController.cs
@@ -25,24 +25,42 @@ namespace Letters.Controllers
         [HttpGet]
         public ActionResult<string> Get()
         {
-             char[] upperCaseLetters = new char[] { 'A','B','C','D','E','F','G','H','Y','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-             char[] lowerCaseLetters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' };
+            //code that works
+            // char[] upperCaseLetters = new char[] { 'A','B','C','D','E','F','G','H','Y','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+            // char[] lowerCaseLetters = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' };
 
-            //string[] malePetNames = new string [] { "Rufus", "Bear", "Dakota", "Fido",
-            //              "Vanya", "Samuel", "Koani", "Volodya",
-            //              "Prince", "Yiska" };
-            //string[] femalePetNames = new string [] { "Maggie", "Penny", "Saya", "Princess",
-            //                "Abby", "Laila", "Sadie", "Olivia",
-            //                "Starlight", "Talla" };
+            //Random rnd = new Random();
 
-            Random rnd = new Random();
+            //int mIndex = rnd.Next(upperCaseLetters.Length );
+            //int fIndex = rnd.Next(lowerCaseLetters.Length);
+            ////var mI = $"{malePetNames[mIndex]}";
+            //var mI = $"{upperCaseLetters[mIndex]}";
+            //var fI = $"{lowerCaseLetters[fIndex]}";
+            //return mI.ToString() + fI.ToString();
 
-            int mIndex = rnd.Next(upperCaseLetters.Length );
-            int fIndex = rnd.Next(lowerCaseLetters.Length);
-            //var mI = $"{malePetNames[mIndex]}";
-            var mI = $"{upperCaseLetters[mIndex]}";
-            var fI = $"{lowerCaseLetters[fIndex]}";
-            return mI.ToString() + fI.ToString();
+            //loop
+            char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            string password = string.Empty;
+            Random random = new Random();
+            int length = 5;
+
+            for (int i = 0; i < length; i++)
+            {
+
+
+                int x = random.Next(1, chars.Length);
+                //Don't Allow Repetation of Characters
+                if (!password.Contains(chars.GetValue(x).ToString()))
+                    password += chars.GetValue(x);
+                else
+                    i--;
+            }
+
+            return password;
+
+
+
+
             //var rnd = new Random();
             //var returnindex = rnd.Next(0, 50);
             //var returnlettersupper = Enumerable.Range('A', 'Z' - 'A' + 1).Select(x => (char)x);
--- code/Numbers/Controllers/NumbersController.cs
+++ code/Numbers/Controllers/NumbersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -19,24 +20,52 @@ namespace Numbers.Controllers
             //};
 
             [HttpGet]
-            public ActionResult<string> Get()
-            {
-            int[] numbers = new int[] { 1,2,3,4,5,6,7,8,9,0 };
-            char[] specialCharacters = new char[] {'$','&','!','£','%','?'};
-            
-            Random rnd = new Random();
-            int nIndex = rnd.Next(numbers.Length);
-            int sIndex = rnd.Next(specialCharacters.Length);
-            //var mI = $"{malePetNames[mIndex]}";
-            var nI = $"{numbers[nIndex]}";
-            var sI = $"{specialCharacters[sIndex]}";
-            return nI.ToString() + sI.ToString();
-            // var rnd = new Random();
-            // var returnIndex = rnd.Next(0, 9);
-            // var numbers = Enumerable.Range('0', '9' - '0' + 1).Select(x => (char)x);
-            // return Numbers[returnIndex].ToString();
-            //return Numbers[numbers].ToString();
-        }
-        }
-}
+        //code that works
+        //public ActionResult<string> Get()
+        //{
+        //int[] numbers = new int[] { 1,2,3,4,5,6,7,8,9,0 };
+        //char[] specialCharacters = new char[] {'$','&','!','£','%','?'};
 
+        //Random rnd = new Random();
+        //int nIndex = rnd.Next(numbers.Length);
+        //int sIndex = rnd.Next(specialCharacters.Length);
+        ////var mI = $"{malePetNames[mIndex]}";
+        //var nI = $"{numbers[nIndex]}";
+        //var sI = $"{specialCharacters[sIndex]}";
+        //return nI.ToString() + sI.ToString();
+
+
+        public ActionResult<string> Get() 
+
+        {
+            char[] chars = "$%#@!*?;:1234567890".ToCharArray();
+            string password = string.Empty;
+            Random random = new Random();
+            int length = 5;
+
+                for (int i = 0; i < length ; i++)
+            {
+
+
+                int x = random.Next(1, chars.Length);
+                //Don't Allow Repetation of Characters
+                if (!password.Contains(chars.GetValue(x).ToString()))
+                    password += chars.GetValue(x);
+                else
+                    i--;
+            }
+
+            return password;
+        }
+        
+    }
+
+
+
+    // var rnd = new Random();
+    // var returnIndex = rnd.Next(0, 9);
+    // var numbers = Enumerable.Range('0', '9' - '0' + 1).Select(x => (char)x);
+    // return Numbers[returnIndex].ToString();
+    //return Numbers[numbers].ToString();
+}
+     
