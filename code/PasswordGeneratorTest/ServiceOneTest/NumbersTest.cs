468d1826c245830b41d9c5e577ae0acc54a41fd9
Yasmin El-Hendi <yasminelhendi@gmail.com>
2021-06-17 13:53:56 +0100

    test

--- code/PasswordGeneratorTest/ServiceOneTest/NumbersTest.cs
+++ code/PasswordGeneratorTest/ServiceOneTest/NumbersTest.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using Numbers.Controllers;
 using System;
 using Xunit;
 
@@ -5,9 +7,17 @@ namespace PasswordGeneratorTest
 {
     public class NumbersTest
     {
-        [Fact]
-        public void Test1()
+        
+    [Fact]
+        public void GetNumber_Test()
         {
+            //Arrange
+            NumbersController numbersController = new NumbersController();
+            //Act
+            var NumbersControllerResult = numbersController.Get();
+            //Assert
+            Assert.NotNull(NumbersControllerResult);
+            Assert.IsType<ActionResult<String>>(NumbersControllerResult);
 
         }
     }
